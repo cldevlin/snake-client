a0bb217b79fb6fac41c60117a34cb0a9b38de273
Claire Devlin <claire.devlin@hotmail.com>
2021-05-05 12:57:44 -0400

    added client file

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",// IP address here,
+    port: 50542// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (input) => {
+    console.log(input);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",// IP address here,
-    port: 50542// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (input) => {
-    console.log(input);
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
