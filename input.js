cd39131858a9f4bd5da64051704e93ce1f681494
Claire Devlin <claire.devlin@hotmail.com>
2021-05-05 14:08:26 -0400

    added custom messages

--- input.js
+++ input.js
@@ -16,6 +16,15 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === '1') {
+    connection.write("Say: Zoooooom");
+  }
+  if (key === '2') {
+    connection.write("Say: helloooooooo");
+  }
+  if (key === '3') {
+    connection.write("Say: beep bop boop");
+  }
 
 };
 
